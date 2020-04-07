a59baa997094bfd89062ac3e2ddd26491aac7a19
makspanshin <makspanshin97@gmail.com>
2020-04-07 22:00:18 +0500

    Add class HorizontalLine

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -6,5 +6,24 @@ namespace Snake
 {
     class Point
     {
+        public int x;
+        public int y;
+        public int sym;
+
+        public Point()
+        {
+
+        }
+        public Point(int _x, int _y , int _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
     }
 }
